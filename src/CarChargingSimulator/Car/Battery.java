8a3fe9d65153d6145d37e708171d2a0c5a431637
faraz2079 <farazchahi2079@gmail.com>
2024-02-02 19:49:58 +0100

    FarazChahybakhsh-7219098-LogsPackage&CarChargingSimulator.Car

--- src/CarChargingSimulator/Car/Battery.java
+++ src/CarChargingSimulator/Car/Battery.java
@@ -1,4 +1,9 @@
 package CarChargingSimulator.Car;
 //battery interface to cars
+
 public interface Battery {
+    double getCapacity();
+    void charge(double amount);
+
+    double getRemaining();
 }
